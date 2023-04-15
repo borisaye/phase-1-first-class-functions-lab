1d58c478337e921e9d8d97eb5b896df48ad5c726
Borisaye <Boris_aroche@hotmail.com>
2023-04-15 00:09:49 -0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(scuberDrivers){
+
+    return scuberDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(scuberDrivers){
+
+    return scuberDrivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multpleOf){
+
+    return function(fare){
+
+        return fare * multpleOf;
+    }
+
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(scuberDrivers, driverSelector){
+
+    return driverSelector(scuberDrivers);
+}
